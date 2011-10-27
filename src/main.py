0abeb3443ecd27ac291bbee204d4bfc16820c39e
Thanathip Limna <boatkrap@gmail.com>
2011-10-27 15:22:19 +0700

    add sample attribute and test call to nokkhum compute

--- src/main.py
+++ src/main.py
@@ -3,9 +3,61 @@ Created on Sep 7, 2011
 
 @author: boatkrap
 '''
+import json
 
 if __name__ == '__main__':
     import json, urllib, urllib2
-    output = urllib2.urlopen('http://localhost:9000/AddValue', urllib.urlencode({'name': '143'}))
+    camera_attribute = {
+        "camera":    {
+            "name":"Camera 1",
+            "model":"Logitech",
+            "fps":10,
+            "width":640,
+            "height":480,
+            "url":"rtsp://172.30.143.249/play1.sdp",
+            "username":"",
+            "password":""
+        },
+
+        "processors":[
+            {
+                "name":"Motion Detector",
+                "interval":3,
+                "resolution":98,
+                "processors":[
+                    {
+                        "name":"Video Recorder",
+                        "fps":10,
+                        "directory":"/tmp",
+                        "width":640,
+                        "height":480,
+                        "record_motion":True,
+                        "maximum_wait_motion":1
+                    },
+                    {
+                        "name":"Face Detector",
+                        "interval":5,
+                        "processors":[
+                            {
+                                "name":"Image Recorder",
+                                "directory":"/tmp",
+                                "width":640,
+                                "height":480
+                            }
+                        ]
+                     }
+                ]
+             },        
+            {
+                "name":"Video Recorder",
+                "fps":10,
+                "directory":"/tmp",
+                "width":640,
+                "height":420,
+            }
+        ]
+    }
+
+    output = urllib2.urlopen('http://localhost:9000/camera/start', urllib.urlencode({'attribute': json.dumps(camera_attribute), 'camera_id':"test-1"}))
     print json.loads(output.read())
     
