ccf9c6b9db55f4e5e3a77ebb24e88a6e8cc6f165
Thanathip Limna <boatkrap@gmail.com>
2011-11-14 16:12:11 +0700

    modify url port

--- src/tests/camera_management.py
+++ src/tests/camera_management.py
@@ -64,46 +64,49 @@ class CameraManagementTest(unittest.TestCase):
         
         import time
         print "try to start camera 1"
-        output = urllib2.urlopen('http://localhost:9000/camera/start', urllib.urlencode({'camera_id':"test-1", 'attributes': json.dumps(camera_attribute)}))
+        camera_attribute["processors"]
+        output = urllib2.urlopen('http://localhost:25531/camera/start', urllib.urlencode({'camera_id':"test-1", 'attributes': json.dumps(camera_attribute)}))
         print json.loads(output.read())
         
         print "sleep 60 1"
         time.sleep(60)
         print "try to start camera 2"
         camera_attribute["camera"]["name"] = "camera 2"
-        output = urllib2.urlopen('http://localhost:9000/camera/start', urllib.urlencode({'camera_id':"test-2", 'attributes': json.dumps(camera_attribute)}))
+        camera_attribute["camera"]["url"] = "rtsp://172.30.142.92/play1.sdp"
+        
+        output = urllib2.urlopen('http://localhost:25531/camera/start', urllib.urlencode({'camera_id':"test-2", 'attributes': json.dumps(camera_attribute)}))
         print json.loads(output.read())
         
         print "sleep 60 2"
         time.sleep(60)
         print "try to start again"
-        output = urllib2.urlopen('http://localhost:9000/camera/start', urllib.urlencode({'camera_id':"test-1", 'attributes': json.dumps(camera_attribute)}))
+        output = urllib2.urlopen('http://localhost:25531/camera/start', urllib.urlencode({'camera_id':"test-1", 'attributes': json.dumps(camera_attribute)}))
         print json.loads(output.read())
     
         print "try to list"
-        output = urllib2.urlopen('http://localhost:9000/camera/list')
+        output = urllib2.urlopen('http://localhost:25531/camera/list')
         print json.loads(output.read())
         
         print "try to get attribute"
-        output = urllib2.urlopen('http://localhost:9000/camera/get_attributes', urllib.urlencode({'camera_id':"test-1"}))
+        output = urllib2.urlopen('http://localhost:25531/camera/get_attributes', urllib.urlencode({'camera_id':"test-1"}))
         print json.loads(output.read())
         
         print "sleep 100"
         time.sleep(100)
         
         print "stop test-1"
-        output = urllib2.urlopen('http://localhost:9000/camera/stop', urllib.urlencode({'camera_id': "test-1"}))
+        output = urllib2.urlopen('http://localhost:25531/camera/stop', urllib.urlencode({'camera_id': "test-1"}))
         print json.loads(output.read())
         
         print "sleep 60 3"
         time.sleep(60)
         
         print "try to list"
-        output = urllib2.urlopen('http://localhost:9000/camera/list')
+        output = urllib2.urlopen('http://localhost:25531/camera/list')
         print json.loads(output.read())
         
         print "stop test-2"
-        output = urllib2.urlopen('http://localhost:9000/camera/stop', urllib.urlencode({'camera_id': "test-2"}))
+        output = urllib2.urlopen('http://localhost:25531/camera/stop', urllib.urlencode({'camera_id': "test-2"}))
         print json.loads(output.read())
 
 
